14a170ea1842fd7fd21dba9185784232d4fa8361
Omar Zuniga <miguel_omar30@hotmail.com>
2025-08-25 21:04:23 -0600

    TRPC Setup

---
+++ src/app/client.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+
+export const Client = () => {
+	const trpc = useTRPC();
+	const { data } = useSuspenseQuery(trpc.createAI.queryOptions({ text: "Antonio" }));
+
+	return <div>{JSON.stringify(data)}</div>;
+};
